b10522e2eb0f9314c71daa06807f52e97dc6c81f
Prakhar14Kumar <Kumarprakhar700@gmail.com>
2024-01-04 15:41:16 +0530

    add

---
+++ HotelBookingApp/src/HotelBookingApp.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class HotelBookingApp {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int c=a+b;
+        }
+}
